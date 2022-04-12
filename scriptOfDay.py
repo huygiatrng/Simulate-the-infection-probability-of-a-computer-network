0150ec07cde1ed03d60b078bf867c116280ba52e
Huy Truong <67343196+huygiatrng@users.noreply.github.com>
2022-04-11 20:18:03 -0400

    Update scriptOfDay.py

--- scriptOfDay.py
+++ scriptOfDay.py
@@ -1,49 +1,41 @@
-import numpy as np
 import distributions
 import random
 
 
-def spreadingVirus(array,infectedprob,nOfRun, nComp): #Attention!!!
-    nInfected = 0
-    nUninfected = 0
-    #count number of infected computers and uninfected
-    for i in range(nComp):
-        if array[i]==1:
-            array[i]=2
-        elif array[i]==0:
-            nUninfected+=1
-    for i in range(nComp):
-        if array[i]==2:
-            nInfected += 1
+def spreadingVirus(list,infectedprob, nComp):
+    nInfected = list.count(1)
     for infected in range(nInfected):
         for i in range(nComp):
-            if array[i] == 0:
+            if list[i] == 0:
                 if distributions.MyBernoulli(infectedprob)==1:
-                     array[i]=1
-    return array
+                    list[i]=1
+    return list
 
-def gettingFixed(array,nFixed,nOfRun,nComp):
-    nInfected = 0
-    nUninfected = 0
-    # count number of infected computers and uninfected
+def gettingFixed(list,nFixed,nComp):
+    nInfected = list.count(1)
     for i in range(nComp):
-        if array[i] == 0:
-            nUninfected += 1
-        elif array[i] == 2:
+        if list[i] == 2:
             nInfected += 1
     if nInfected <nFixed:
         nFixed = nInfected
     #Random pick number of infected to fixed
-    indexToFixed=random.sample(range(0, nInfected), nFixed)
+    indexToFixed = random.sample(range(0, nInfected), nFixed)
     indexToFixed.sort()
     count = 0
     index = 0
     for i in range(nComp):
-        if array[i]==2:
+        if list[i]==1:
             if index >=nFixed:
                 break
             elif count == indexToFixed[index]:
                 index+=1
-                array[i]=0
+                list[i]=0
             count+=1
-    return array
+    return list
+
+def beInfectedAtleastOnceList(list,answer):
+    for i in range(len(list)):
+        if list[i]==1:
+            answer[i]=1
+    return answer
+
