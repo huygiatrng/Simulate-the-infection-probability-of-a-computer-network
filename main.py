57bb4ac6e31a7567c431f8e91f61b71e8f61c4ca
Huy Truong <67343196+huygiatrng@users.noreply.github.com>
2022-04-11 20:17:31 -0400

    update mainn

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import distributions
 import os
-import numpy as np
-import random
 import scriptOfDay
 from matplotlib import pyplot as plt
+import statistics
+import graphdata
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -12,11 +11,25 @@ def enterAndCheckInput(n):
     checkInput = True
     while checkInput == True:
         if n==1:
-            data = input("Enter number of computers ")
+            data = input("Enter number of computers: ")
+            while int(data)<0:
+                print("Invalid value!")
+                data = input("Enter number of computers: ")
         elif n==2:
-            data = input("Enter percentage of computer infected:(%)")
+            data = input("Enter percentage of computer infected(%): ")
+            while int(data)<0:
+                print("Invalid value!")
+                data = input("Enter percentage of computer infected(%): ")
         elif n==3:
-            data = input("Enter number of computers repaired daily:")
+            data = input("Enter number of computers repaired daily: ")
+            while int(data)<0:
+                print("Invalid value!")
+                data = input("Enter number of computers repaired daily: ")
+        elif n==4:
+            data = input("Enter number of iterations: ")
+            while int(data)<0:
+                print("Invalid value!")
+                data = input("Enter number of iterations: ")
         try:
             returndata = int(data)
             checkInput = False
@@ -25,7 +38,6 @@ def enterAndCheckInput(n):
     return returndata
 
 def enterInput():
-
     ncomp = enterAndCheckInput(1)
     if ncomp == 0:
         ncomp = 20
@@ -35,47 +47,64 @@ def enterInput():
     nFixed = enterAndCheckInput(3)
     if nFixed == 0:
         nFixed = 5
-    return [ncomp,prob,nFixed]
+    nOfRun = enterAndCheckInput(4)
+    if nOfRun == 0:
+        nOfRun = 10000
+    return [ncomp,prob,nFixed,nOfRun]
+
 
 cls()
-print("Default:\n     number of computers:20, percentage of computer infected:10%, number of computers repaired daily:5\n     (*)You can use default values by entering 0 as their inputs.\n-----------------------------------------------------------------------------------------------------")
-arrayInput = enterInput()
-print(arrayInput)
+print("Default:\n     number of computers:20, percentage of computer infected:10%, number of computers repaired daily:5, number of literations: 10000\n     (*)You can use default values by entering 0 as their inputs.\n-----------------------------------------------------------------------------------------------------")
+ncomp,prob,nFixed,nOfRun = enterInput()
 cls()
-#Number of run:
-nOfRun = 10000
 print("\n")
 
-#Create a array has nOfRun of numperOfComputer (nComp)
-#Let 0= uninfected, 1 = will infected, 2 = infected
-array_2d = np.zeros((nOfRun, arrayInput[0]))
-for i in range(nOfRun):
-    array_2d[i][0]=2
-#print(array_2d)
+#Create a list has nOfRun of numperOfComputer (nComp)
+#Let 0= uninfected, 1 = infected
 
-checkAllofArray = np.count_nonzero(array_2d ==0)
-#print(checkAllofArray)
-dayCount = 0
-dayToFinishFullFix = np.zeros((nOfRun,1))
-countFinished = 0
-while checkAllofArray<arrayInput[0]*nOfRun:
-    checkNUninfected = np.count_nonzero(array_2d ==0, axis=1)
-    dayCount+=1
-    print("\nDay: ",dayCount," -----------------")
-    for i in range(nOfRun):
-        if checkNUninfected[i]!=arrayInput[0]:
-            #Now run everyday script
-            array_2d[i] = scriptOfDay.spreadingVirus(array_2d[i],arrayInput[1],nOfRun,arrayInput[0])
-            array_2d[i] = scriptOfDay.gettingFixed(array_2d[i], arrayInput[2], nOfRun, arrayInput[0])
-        else:
-            if dayToFinishFullFix[i]==0:
-                countFinished +=1
-                dayToFinishFullFix[i] = dayCount
-                #print(dayToFinishFullFix[i])
-                print(countFinished)
-    checkAllofArray = np.count_nonzero(array_2d == 0)
+dayToFinish = []
 
-plt.hist(dayToFinishFullFix)
-plt.show()
+infectedAtLeastOnceList = []
 
-print("Average day to fix all:",np.mean(dayToFinishFullFix))
+expectedInfectedComputerList = []
+
+print("Simulating...")
+
+for iters in range(nOfRun):
+    list = [0] * ncomp
+    list[0] = 1
+    #print("Iteration:",iters)
+    beInfectedAtLeastOnce = [0] * ncomp
+    daycount = 1
+    countInfectedPerday = []                        #list of number of computers are infected in everyday.
+    while list.count(1)!=0:
+        #morning
+        list = scriptOfDay.spreadingVirus(list, prob, ncomp)
+        # check if computers were infected at least one.
+        beInfectedAtLeastOnce = scriptOfDay.beInfectedAtleastOnceList(list, beInfectedAtLeastOnce)
+        # compute number of computers are infected:
+        countInfectedPerday.append(list.count(1))                       #number of computers are infected.
+        #evening
+        list = scriptOfDay.gettingFixed(list, nFixed, ncomp)
+        daycount += 1
+    probInfectedAtLeastOnce = beInfectedAtLeastOnce.count(1)/ncomp
+    infectedAtLeastOnceList.append(probInfectedAtLeastOnce)
+    expected = statistics.mean(countInfectedPerday)
+    expectedInfectedComputerList.append(expected)
+    dayToFinish.append(daycount)
+    #Print time to finish in this loop
+    #print("Finished in ",daycount," days.")
+
+cls()
+#Print the results
+print("A. Average day to fix all:", statistics.mean(dayToFinish))
+print("B. Probability that each computer gets infected at least once: ",statistics.mean(infectedAtLeastOnceList))
+print("C. The expected number of computers that get infected:", statistics.mean(expectedInfectedComputerList))
+
+
+#Show the histogram of days to finish of literations.
+graphdata.histogramData(dayToFinish,0)
+#-----------------------------------------------------------------------------
+#graphdata.histogramData(infectedAtLeastOnceList,1)
+#-----------------------------------------------------------------------------
+#graphdata.histogramData(expectedInfectedComputerList,2)
